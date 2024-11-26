ad250ebe9b9da436e7a4dbcba07054eea985d400
Hussain Tarrada <hussaintarrada@Hussains-MacBook-Air.local>
2024-11-26 21:02:24 +0300

    W03-D01-DOMEvent-LAB

--- app.js
+++ app.js
@@ -1,78 +1,76 @@
-let num1 = 0
-let num2 = 0
-let num3 = 0
-let totalNum = 0
-const buttons = document.querySelectorAll('.button');
+let num1 = ""
+let operator 
+let num2 = ""
+let totalNum 
+const numberButtons = document.querySelectorAll('.number');
 
-buttons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    // This log is for testing purposes to verify we're getting the correct value
-    console.log(event.target.innerText);
-    // Future logic to capture the button's value would go here...
-    let firstButton = event.target.innerText
-    if(Number(firstButton) !== NaN){
-      num1 = Number(firstButton)
-    }
-    else{
-      num1 = 0
-    }
-  });
-  button.addEventListener('click', (event) => {
-    let secondButton = event.target.innerText
-    if(Number(secondButton) === NaN){
-      if(secondButton === "*"){
-        num2 = 1
-      }
-      else if(secondButton === "/"){
-        num2 = 2
-      }
-      else if(secondButton === "+"){
-        num2 = 3
-      }
-      else if(secondButton === "-"){
-        num2 = 4
-      }
-      else{
-        num2 = 0
-      }
-    }
-    else{
-      num2 = 0
-    }
-  })
-  button.addEventListener('click', (event) => {
-  let thirdButton = event.target.innerText
-    if(Number(thirdButton) !== NaN){
-      num3 = Number(thirdButton)
-    }
-    else{
-      num3 = 0
-    }
-  })
-  button.addEventListener('click', (event) => {
-    let fourthButton = event.target.innerText
-    if(fourthButton === "="){
-      if(num2 = 1){
-        totalNum = num1 * num3
-        console.log(totalNum)
-      }
-      else if(num2 = 2){
-        totalNum = num1 / num3
-        console.log(totalNum)
-      }
-      else if(num2 = 3){
-        totalNum = num1 + num3
-        console.log(totalNum)
-      }
-      else if(num2 = 2){
-        totalNum = num1 - num3
-        console.log(totalNum)
-      }
-    }
-    else{
-      num1 = 0
-      totalNum = 0
-    }
-  })
-});
+const operatorButton = document.querySelectorAll('.operator');
 
+const equalButton = document.querySelector('.equals')
+
+const display = document.querySelector('.display')
+
+numberButtons.forEach((button)=> {
+  button.addEventListener("click",(event) => {
+    if(display.innerText === "" && !operator){
+      num1 = event.target.innerText
+      display.innerText = event.target.innerText
+    }
+    else if(display.innerText !== "" && !operator){
+      num1 = num1 + event.target.innerText
+      display.innerText = num1
+      console.log(num1)
+    }
+    else if(num1 === "" && operator){
+      num2 = event.target.innerText
+      display.innerText= num2
+      console.log(num2)
+    }
+    else if(num1 !== "" && operator){
+      num2 = num2 + event.target.innerText
+      display.innerText = num2
+    }
+  })
+})
+
+
+operatorButton.forEach((button) => {
+  button.addEventListener("click",(event) => {
+    if(display.innerText && display.innerText !== "C"){
+      operator = event.target.innerText
+      display.innerText = operator
+      console.log(operator)
+    }
+    else if(event.target.innerText === "C"){
+       display.innerText = ""
+       num1 = ""
+       num2 = ""
+       totalNum = 0    
+    }
+    else{
+      return
+    }
+  })
+})
+
+equalButton.addEventListener("click",(event) => {
+  num1 = Number(num1)
+  num2 = Number(num2)
+  if(operator === "*"){
+    totalNum = num1 * num2
+    display.innerText = totalNum
+  }
+  else if(operator === "/"){
+    totalNum = num1 / num2
+    display.innerText = totalNum
+  }
+  else if(operator === "+"){
+    totalNum = num1 + num2
+    display.innerText = totalNum
+  }
+  else if(operator === "-"){
+    totalNum = num1 - num2
+    display.innerText = totalNum
+  }
+
+})
